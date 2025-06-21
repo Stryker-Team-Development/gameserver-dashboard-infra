dd640226fcaa30ddec17d8eb71c934a7c693ec12
Alejandro Ochoa <alejo8a_2@hotmail.com>
2025-06-21 18:43:39 -0500

    Update version file, remove hardcoded profiles

--- versions.tf
+++ versions.tf
@@ -5,9 +5,15 @@ terraform {
       version = ">= 5.0.0"
     }
   }
+
+  backend "s3" {
+    bucket = "olimpo-platform-terraform-states"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+
 }
 
 provider "aws" {
   region  = "us-east-1"
-  profile = "jose.alvarez"
 }
